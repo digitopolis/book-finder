56db534699e167299c48a06ccae0c4a362d84040
Matt Readout <matt.readout@gmail.com>
2019-11-07 18:33:05 -0500

    display results after search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 const getBooks = require('../lib/apiCommands')
+const displayResults = require('../lib/userInterface')
+
 let searchResults = []
+let userReadingList = []
 
 const updateSearchResults = (newBooks) => {
 	searchResults = newBooks
-	console.log(searchResults);
+	displayResults(searchResults)
 }
 
 console.log('find a book!');
