de439eb9f3fa41096e47e99c63462a1721886e1c
Matt Readout <matt.readout@gmail.com>
2019-11-07 17:38:14 -0500

    add updateSearchResults

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 const getBooks = require('../lib/apiCommands')
+let searchResults = []
+
+const updateSearchResults = (newBooks) => {
+	searchResults = newBooks
+	console.log(searchResults);
+}
 
 console.log('find a book!');
-console.log(getBooks('cats'));
+getBooks('cats').then(updateSearchResults)
