fefcc7ab7825af72a0f70bd0aecd63e6eae55abf
Matt Readout <matt.readout@gmail.com>
2019-11-08 17:02:06 -0500

    build main app logic

--- src/index.js
+++ src/index.js
@@ -11,5 +11,26 @@ const updateSearchResults = (newBooks) => {
 	displayResults(searchResults)
 }
 
-console.log('find a book!');
-getBooks('cats').then(updateSearchResults)
+const app = async () => {
+	let step = 'start'
+	let searchTerm = ''
+	while (step !== 'exit') {
+		if (step === 'start') {
+			welcome()
+			step = 'search'
+		} else if (step === 'search') {
+			searchTerm = getSearchTerm()
+			console.log(`Searching for "${searchTerm}". . .`)
+			const books = await getBooks(searchTerm)
+			updateSearchResults(books)
+			step = 'submit'
+		} else if (step === 'submit' && resultsDisplayed) {
+			step = menuSelection()
+		} else if (parseInt(step) >= 1 && parseInt(step) <= 5) {
+			console.log(`you selected ${step}`)
+			step = 'exit'
+		}
+	}
+}
+
+app()
